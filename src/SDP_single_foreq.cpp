36284d4a7282ada7e06441104a0038fd999b6985
jdyeakel <jdyeakel@gmail.com>
2014-11-03 09:55:25 -0700

    state variable updating

--- src/SDP_single_foreq.cpp
+++ src/SDP_single_foreq.cpp
@@ -12,7 +12,7 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
    xc = xc -1; //Index
    double xc_state = (double) xc; //state
    
-   
+   //Book-keeping
    //Establish iniital variables required for the SDP
    int num_res = res_bs.size();
    //How many decisions? Count columns on first decision matrix
@@ -27,6 +27,10 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
    
    //Which of these individuals are alive (they all start out alive)
    IntegerVector alive(N);
+   for (int n=0; n<N; n++) {
+     alive(n) = 1;
+   }
+   num_alive = sum(alive);
    
    //This records the energetic state of each individual... values will change over time
    IntegerVector state_v(N);
@@ -60,9 +64,19 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
    //Begin time iteration (this is the simulation time)
    for (int t=0; t<tsim; t++) {
      
+     //Book-keeping
      //Reset metrics
      //How many individuals are currently alive?
-     int N = sum(alive);
+     int N = num_alive;
+     
+     //Create vector to record reproductive events (1,0)
+     IntegerVector rep_success(N);
+     
+     //To save the 'next' values for the energetic vector
+     IntegerVector x_next_rd(N);
+     
+     //To save the 'next' resouce consumed
+     IntegerVector res_next(N);
      
      //Begin Individual iterations... Note: N will change with each t
      for (int n=0; n<N; n++) {
@@ -77,13 +91,15 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
        //What is the probability that the individual will reproduce?
        rep_prob = rep_gain(ind_x);
        //Does the individual reproduce in this timestep?
+       //Take care of reproduction after n iterations... to include density dependence
        NumericVector rdraw_v = runif(1);
        double rdraw = as<double>(rdraw_v);
        if (rdraw < rep_prob) {
-         int rep.success = 1;
+         int rep_success(n) = 1;
        } else {
-         int rep.success = 0;
+         int rep_success(n) = 0;
        }
+       
        
        //Choose next resource as a function of the preference probability distribution
        NumericMatrix pref_prob_m = dec_ls(ind_r);
@@ -108,7 +124,7 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
        //Random draw between 0 and tot_num
        int draw = (int) floor(rdraw*(tot_num));
        //This defines the next resource (the index for the next resource)
-       int res_next = p_bin(draw);
+       res_next(n) = p_bin(draw);
        
        //How many of this resource is found?
        NumericVector k_prob(max_enc);
@@ -146,13 +162,15 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
        
        //Energetic dynamics
        if (forage_success == 1) {
+         //If food is captured
          int x_next = ind_x + eta*g_forage(res_next) - c_forage(res_next);
        } else {
+         //If food is not captured
          int x_next = ind_x - c_forage(res_next);
        }
        
        //Turn x_next into nearest integer
-       int x_next_rd = round(x_next);
+       x_next_rd(n) = round(x_next);
        
        //Boundary conditions and determine whether individual dies
        if (x_next_rd < xc_state) {
@@ -164,16 +182,44 @@ NumericVector g_forage, NumericVector c_forage, List W_nr, List istar_nr, int N,
          alive(n) = 1;
        }
        
-       
-       
-       
-       
-       
-       
-       
-       
-       
      } //end individual iterations
+     
+     //Density-dependent Reproduction :: TODO
+     //Must add on new individuals to the current state vectors
+     
+     
+     
+     //How many individuals survive this timestep?
+     int num_alive = sum(alive);
+     
+     //Redefine states only for alive individuals
+     IntegerVector state_v(num_alive);
+     IntegerVector time_v(num_alive);
+     IntegerVector res_v(num_alive);
+     IntegerVector dec_v(num_alive);
+     
+     int tic = 0;
+     for (i=0; i<N; i++) {
+       if (alive(tic) == 1) {
+
+         //Updating states for next t iteration
+         //This records the energetic state of each individual... values will change over time
+         state_v(tic) = xmax; //The initial state is full health
+         
+         //Record the time vector for each individual
+         time_v(tic) = 1; //The intitial time is 1 of course
+         
+         //Record the next resource for each individual
+         res_v(tic) = res_next(i); //The initial resource is randomly drawn for each individual
+         
+         //What are the optimal decisions for the current individual-level states?
+         istar_t = istar_nr(res_v(tic)); //Grab the istar for the focal resource
+         dec_v(tic) = istar_t(state_v(i),0); //Grab the decision for a given state and time t=0;
+         
+         int tic = tic + 1;
+       } //end if-alive statement
+     } //end i
+     
      
    } //end time iterations
    
